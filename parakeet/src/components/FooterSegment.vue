ce6ae6e79f1ec5bf2e6b9588eecf3ec842299020
justrdk <osman.sist@gmail.com>
2017-05-28 02:15:18 -0600

    fixed footer structure, added icons

--- parakeet/src/components/FooterSegment.vue
+++ parakeet/src/components/FooterSegment.vue
@@ -5,8 +5,8 @@
       <div class="stackable ui four column grid">
         <div class="row">
           <div class="column">
-            <h4 class="ui header Footer--title">Jobs at</h4>
-            <div class="ui list Footer--list">
+            <h4 class="ui header Footer--title align-left">Jobs at</h4>
+            <div class="ui list Footer--list align-left">
               <div class="item">Home</div>
               <div class="item">Jobs</div>
               <div class="item">About</div>
@@ -14,8 +14,8 @@
             </div>
           </div>
           <div class="column">
-            <h4 class="ui header Footer--title">Information</h4>
-            <div class="ui list Footer--list">
+            <h4 class="ui header Footer--title align-left">Information</h4>
+            <div class="ui list Footer--list align-left">
               <div class="item">F.A.Q.</div>
               <div class="item">Application Process</div>
               <div class="item">Privacy Policy</div>
@@ -23,18 +23,30 @@
             </div>
           </div>
           <div class="column">
-            <h4 class="ui header Footer--title">Follow Us</h4>
-            <div class="ui list Footer--list">
-              <div class="item"><i class="users icon"></i> <div class="content">Newsletter</div></div>
-              <div class="item"><i class="users icon"></i><div class="content">Instagram</div></div>
-              <div class="item"><i class="users icon"></i><div class="content">Twitter</div></div>
-              <div class="item"><i class="users icon"></i><div class="content">Tumblr</div></div>
-              <div class="item"><i class="users icon"></i><div class="content">Linkedin</div></div>
+            <h4 class="ui header Footer--title align-left">Follow Us</h4>
+            <div class="ui two column grid align-left">
+            <div class="column">
+              <div class="ui list Footer--list">
+                <div class="item"><i class="circular small users icon"></i> <div class="content">Newsletter</div></div>
+                <div class="item"><i class="circular small instagram icon"></i><div class="content">Instagram</div></div>
+                <div class="item"><i class="circular small twitter icon"></i><div class="content">Twitter</div></div>
+                <div class="item"><i class="circular small tumblr icon"></i><div class="content">Tumblr</div></div>
+                <div class="item"><i class="circular small linkedin icon"></i><div class="content">Linkedin</div></div>
+              </div>
+            </div>
+            <div class="column">
+              <div class="ui list Footer--list">
+                <div class="item"><i class="circular small facebook f icon"></i> <div class="content">Facebook</div></div>
+                <div class="item"><i class="circular small pinterest icon"></i><div class="content">Pinterest</div></div>
+                <div class="item"><i class="circular small youtube icon"></i><div class="content">Youtube</div></div>
+                <div class="item"><i class="circular small google icon"></i><div class="content">Google</div></div>
+              </div>
+            </div>
             </div>
           </div>
           <div class="column">
-            <h4 class="ui header Footer--title">Job Alert</h4>
-            <span class="Footer--item">Subscribe to our job alert</span>
+            <h4 class="ui header Footer--title align-left">Job Alert</h4>
+            <span class="Footer--subtitle align-left">Subscribe to our job alert</span>
           </div>
         </div>
       </div>
@@ -48,15 +60,17 @@ export default {
 </script>
 <style lang="scss" scoped>
 .Footer {
-  &--title {
-    font-family: 'Open Sans', sans-serif !important;
+  &--title, &--subtitle, &--list, &--item {
+    font-family: 'Open Sans', sans-serif;
     font-size: 12px;
     letter-spacing: 0.5px;
   }
-  &--list, &--item {
+  &--subtitle {
+    color: #979797;
     font-size: 12px;
-    font-family: 'Open Sans', sans-serif;
-    letter-spacing: 0.5px;
+    float: left;
+  }
+  &--list, &--item {
     color: #979797;
   }
 }
